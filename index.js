30b2d9fb4d1baa1aa6ae26741ab88dd060ad1bcd
Rawan Elkhouly <52850403+rawaann@users.noreply.github.com>
2021-05-19 04:23:49 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const {app} = require('./app')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://rawanelkhouly:fairytale@cluster0.pya8q.mongodb.net/BachelorProject?retryWrites=true&w=majority',
-{useNewUrlParser: true, useUnifiedTopology: true}).then(console.log("pass"))
-mongoose.set('useNewUrlParser', true)
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
-
-app.listen(3000)
